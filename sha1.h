fed595748d190b36e8e32ab9b7d99ae75e5309d2
winxuan <mawenxuan963@gmail.com>
2025-06-12 12:50:55 +0800

    Fix: Replaced u_int32_t with uint32_t for Windows compatibility
    This commit replaces instances of `u_int32_t` with `uint32_t` to ensure compatibility with the Windows platform. 
    
    The changes standardize the code to use types defined in `<stdint.h>`, which are widely supported across different platforms, improving cross-platform portability and build consistency.

--- sha1.h
+++ sha1.h
@@ -23,20 +23,20 @@
  */
 #pragma once
 
-#include <sys/types.h>
+#include <stdint.h>  // 替换<sys/types.h>，确保uint32_t定义
 
 namespace file_block_digest {
 typedef struct {
   union {
-    u_int32_t h0;  // 兼容openssl SHA_CTX结构
-    u_int32_t state[5];
+    uint32_t h0;  // 替换u_int32_t为uint32_t
+    uint32_t state[5];
   };
-  u_int32_t count[2];
+  uint32_t count[2];
   unsigned char buffer[64];
 } SHA_CTX;
 
 void SHA1_Init(SHA_CTX* context);
-void SHA1_Update(SHA_CTX* context, const unsigned char* data, u_int32_t len);
+void SHA1_Update(SHA_CTX* context, const unsigned char* data, uint32_t len);
 void SHA1_Final(unsigned char digest[20], SHA_CTX* context);
 
-}  // namespace file_block_digest 
+}  // namespace file_block_digest
